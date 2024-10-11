7a6dec8c73a5094095e2cf86bd8e92b9ccb8ef4a
Samavi <alisamav1213@gmail.com>
2024-10-11 20:48:11 +0330

    feat(entities): add Product entity to represent product details
    - Created a Product class in the entities folder.
    - Added fields for key, id, name, and price to store product information.
    - Included a constructor for initializing product instances.

---
+++ lib/domain/entities/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  late int? key;
+  late int id;
+  late String name;
+  late int price;
+
+  Product({
+    this.key,
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+}
