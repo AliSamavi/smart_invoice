1a8e027abc634fd49474a403cf96ea36436d4b44
Samavi <alisamav1213@gmail.com>
2024-10-18 22:28:18 +0330

    fix(locals): ensure Hive box is fully initialized before performing operations
    - Added _completer variable to ProductLocalImpl to wait for Hive box initialization.
    - Updated all CRUD operations to await the _completer's completion, ensuring safe access to the Hive box.

--- lib/data/sources/locals/product_local.dart
+++ lib/data/sources/locals/product_local.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:smart_invoice/data/models/product/product_model.dart';
@@ -11,10 +13,12 @@ abstract class ProductLocal {
 
 class ProductLocalImpl extends GetxController implements ProductLocal {
   late Box<ProductModel> _hiveBox;
+  final _completer = Completer<void>();
 
   @override
   void onInit() async {
     _hiveBox = await Hive.openBox<ProductModel>("products");
+    _completer.complete();
     super.onInit();
   }
 
@@ -26,21 +30,25 @@ class ProductLocalImpl extends GetxController implements ProductLocal {
 
   @override
   Future<List<ProductModel>> getProducts() async {
+    await _completer.future;
     return _hiveBox.values.toList();
   }
 
   @override
   Future<int> addProduct(ProductModel product) async {
+    await _completer.future;
     return _hiveBox.add(product);
   }
 
   @override
   Future<void> editProduct(int key, ProductModel product) async {
+    await _completer.future;
     await _hiveBox.put(key, product);
   }
 
   @override
   Future<void> deleteProduct(int key) async {
+    await _completer.future;
     await _hiveBox.delete(key);
   }
 }
