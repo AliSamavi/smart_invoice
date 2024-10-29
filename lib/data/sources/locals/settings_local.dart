8d797689f152af8a0efc81cbc23c923e289c94b9
Samavi <alisamav1213@gmail.com>
2024-10-29 16:40:13 +0330

    feat(locals): add SettingsLocalImpl for managing app settings with Hive
    - Created SettingsLocalImpl as a GetxController to handle settings retrieval and modification.
    - Integrated Hive for local storage of settings with lazy initialization and cleanup.
    - Added a Completer to ensure Hive box is initialized before accessing or modifying data.

---
+++ lib/data/sources/locals/settings_local.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:smart_invoice/data/models/settings/settings_model.dart';
+
+abstract class SettingsLocal {
+  Future<SettingsModel?> getSettings();
+  Future<void> editSettings(SettingsModel settings);
+}
+
+class SettingsLocalImpl extends GetxController implements SettingsLocal {
+  late Box<SettingsModel> _hiveBox;
+  final _completer = Completer<void>();
+
+  @override
+  void onInit() async {
+    _hiveBox = await Hive.openBox<SettingsModel>("settings");
+    _completer.complete();
+    super.onInit();
+  }
+
+  @override
+  void onClose() async {
+    await _hiveBox.close();
+    super.onClose();
+  }
+
+  @override
+  Future<SettingsModel?> getSettings() async {
+    await _completer.future;
+    return _hiveBox.values.firstOrNull;
+  }
+
+  @override
+  Future<void> editSettings(SettingsModel settings) async {
+    await _completer.future;
+    await _hiveBox.put(0, settings);
+  }
+}
