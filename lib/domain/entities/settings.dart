44465d7c91af4cc2c3f36b1c0ef6182584ec9ac2
Samavi <alisamav1213@gmail.com>
2024-10-30 12:09:28 +0330

    feat(entities): add Settings entity with nullable fields for configuration data
    - Introduced Settings entity with nullable properties for storing optional user configuration details.

---
+++ lib/domain/entities/settings.dart
@@ -0,0 +1,19 @@
+class Settings {
+  final String? name;
+  final String? province;
+  final String? city;
+  final String? address;
+  final int? phoneNum;
+  final int? postalCode;
+  final int? economicNum;
+
+  Settings({
+    this.name,
+    this.province,
+    this.city,
+    this.address,
+    this.phoneNum,
+    this.postalCode,
+    this.economicNum,
+  });
+}
