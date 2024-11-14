c5e58fd4dc769087a532ed8a6298d2f06075c12d
Samavi <alisamav1213@gmail.com>
2024-11-14 13:51:47 +0330

    feat(routes): add settings route to Routes class
    - Added a new route for the settings page
    - Defined `settings` path and linked it with `SettingsPage` and `SettingsBinding`

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,15 +1,23 @@
 import 'package:get/get.dart' show GetPage;
 import 'package:smart_invoice/presentation/bindings/main_binding.dart';
+import 'package:smart_invoice/presentation/bindings/settings_binding.dart';
 import 'package:smart_invoice/presentation/pages/main_page.dart';
+import 'package:smart_invoice/presentation/pages/settings_page.dart';
 
 class Routes {
   static const String main = "/";
+  static const String settings = "/settings";
 
   static List<GetPage> get pages => [
         GetPage(
           name: main,
           page: () => const MainPage(),
           binding: MainBinding(),
+        ),
+        GetPage(
+          name: settings,
+          page: () => const SettingsPage(),
+          binding: SettingsBinding(),
         ),
       ];
 }
