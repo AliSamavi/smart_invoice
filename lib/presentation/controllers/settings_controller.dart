777c2fe8dbfa819befe848d65eadda6ec9801693
Samavi <alisamav1213@gmail.com>
2024-11-14 19:50:03 +0330

    feat(settings): add SettingsController for managing user settings state
    - Initialized SettingsController to handle settings retrieval and editing
    - Added validation for required fields and login functionality
    - Updated `onInit` to load settings and `onClose` to save settings

--- lib/presentation/controllers/settings_controller.dart
+++ lib/presentation/controllers/settings_controller.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:smart_invoice/config/routes.dart';
 import 'package:smart_invoice/domain/entities/settings.dart';
 import 'package:smart_invoice/domain/usecases/settings/edit_settings.dart';
 import 'package:smart_invoice/domain/usecases/settings/get_settings.dart';
@@ -16,30 +18,104 @@ class SettingsController extends GetxController {
     _editSettings = editSettings;
   }
 
-  final Rxn<Settings> _settings = Rxn<Settings>();
-  Settings? get settings => _settings.value;
+  late Settings _settings;
+
+  final name = TextEditingController();
+  final address = TextEditingController();
+  final phoneNum = TextEditingController();
+  final postalCode = TextEditingController();
+  final economicNum = TextEditingController();
+  final registrationNum = TextEditingController();
+  final nationalNum = TextEditingController();
+
+  TextEditingController province = TextEditingController();
+  TextEditingController city = TextEditingController();
+
+  RxnString signature = RxnString();
+  RxnString stamp = RxnString();
+  RxnString logo = RxnString();
+
+  bool get authChack => Hive.box("").get(0) == null;
 
   @override
-  void onInit() {
-    _onGetSettings();
-    super.onInit();
-  }
-
-  void _onGetSettings() async {
+  void onInit() async {
     final getResult = await _getSettings();
     getResult.fold(
       (failure) => debugPrint(failure.message),
-      (settings) => _settings(settings),
+      (settings) {
+        _settings = settings;
+        name.text = settings.name ?? "";
+        address.text = settings.address ?? "";
+        phoneNum.text = settings.phoneNum?.toString() ?? "";
+        postalCode.text = settings.postalCode?.toString() ?? "";
+        economicNum.text = settings.economicNum?.toString() ?? "";
+        registrationNum.text = settings.registrationNum?.toString() ?? "";
+        nationalNum.text = settings.nationalNum?.toString() ?? "";
+
+        province.text = settings.province ?? "";
+        city.text = settings.city ?? "";
+
+        signature(settings.signature);
+        stamp(settings.stamp);
+      },
     );
+    super.onInit();
   }
 
-  void onEditSettings() async {
-    final editResult = await _editSettings.call(_settings.value!);
+  @override
+  void onClose() async {
+    final editResult = await _editSettings.call(
+      Settings(
+        name: name.text.isEmpty ? _settings.name : name.text,
+        province: province.text.isEmpty ? _settings.province : province.text,
+        city: city.text.isEmpty ? _settings.city : city.text,
+        address: address.text.isEmpty ? _settings.address : address.text,
+        phoneNum: phoneNum.text.isEmpty ? _settings.phoneNum : phoneNum.text,
+        postalCode:
+            postalCode.text.isEmpty ? _settings.postalCode : postalCode.text,
+        economicNum:
+            economicNum.text.isEmpty ? _settings.economicNum : economicNum.text,
+        registrationNum: registrationNum.text.isEmpty
+            ? _settings.registrationNum
+            : registrationNum.text,
+        nationalNum:
+            nationalNum.text.isEmpty ? _settings.nationalNum : nationalNum.text,
+        signature: signature.value,
+        stamp: stamp.value,
+      ),
+    );
     editResult.fold(
       (failure) => debugPrint(failure.message),
       (_) {
         debugPrint("edited successfully");
       },
     );
+    super.onClose();
+  }
+
+  void login() {
+    List<String> missingFields = [];
+
+    if (name.text.isEmpty) missingFields.add("نام");
+    if (province.text.isEmpty) missingFields.add("استان");
+    if (city.text.isEmpty) missingFields.add("شهر");
+    if (address.text.isEmpty) missingFields.add("آدرس");
+    if (phoneNum.text.isEmpty) missingFields.add("شماره تلفن");
+
+    if (missingFields.isEmpty) {
+      Get.offAndToNamed(Routes.main);
+      Hive.box("").add(true);
+    } else {
+      Get.snackbar(
+        "",
+        missingFields.join("، "),
+        snackPosition: SnackPosition.BOTTOM,
+        titleText: Text(
+          "لطفا فیلد${missingFields.length > 1 ? "های" : ""} زیر را کامل کنید",
+          style: const TextStyle(fontWeight: FontWeight.w500),
+        ),
+        margin: const EdgeInsets.all(25),
+      );
+    }
   }
 }
